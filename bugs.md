df501a1f9e56d1e1a2fd57d1d7cbb7ecb145f424
Sife-Cleak <sifed@qq.com>
2024-07-23 21:33:31 +0800

    Add Headers

--- bugs.md
+++ bugs.md
@@ -1,7 +1,7 @@
 Bugs:
 DOM
 WebSocket
-HTTP响应无法发送全部标头（目前只能发送Content-type）
+//HTTP响应无法发送全部标头（目前只能发送Content-type）
 CSS url()函数
 window.open/ location
 
--- index.js
+++ index.js
@@ -40,14 +40,14 @@ main=async function(uri,req,res){
   var head="";
   var lock=1;
   return await fetch(uri,{method:req.method,headers:hhf,body:req.body}).then(r=>{
-    var headall=[...r.headers.entries()].reduce((obj, [key, value]) => (obj[key] = value, obj), {});
-    if(headall["content-type"]){
-      var fh={"content-type":[...r.headers.entries()].reduce((obj, [key, value]) => (obj[key] = value, obj), {})["content-type"]}
+    var fh=Object.fromEntries(r.headers)//[...r.headers.entries()].reduce((obj, [key, value]) => (obj[key] = value, obj), {});
+    /*if(headall["content-type"]){
+      var fh=//{"content-type":[...r.headers.entries()].reduce((obj, [key, value]) => (obj[key] = value, obj), {})["content-type"]}
     }else{
       fh={};
-    }
+    }*/
     //var fh=Object.fromEntries([...r.headers.entries()])
-    if(req.url.search("baidu.com")!=-1){console.log([...r.headers.entries()].reduce((obj, [key, value]) => (obj[key] = value, obj), {}))}
+    //if(req.url.search("baidu.com")!=-1){console.log([...r.headers.entries()].reduce((obj, [key, value]) => (obj[key] = value, obj), {}))}
         res.writeHead(r.status,fh)
         if(r.headers.get("location")){
           console.log(r.headers)
